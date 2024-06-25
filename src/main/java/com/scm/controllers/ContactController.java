5ad53ea24ff8ec57324f2279c7c1e1235c4e1530
Shivam Pawar <shivampawar0014@gmail.com>
2024-06-25 13:08:24 +0530

    validation contact

--- src/main/java/com/scm/controllers/ContactController.java
+++ src/main/java/com/scm/controllers/ContactController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,56 +14,67 @@ import com.scm.entities.Contact;
 import com.scm.entities.User;
 import com.scm.forms.ContactForm;
 import com.scm.helpers.Helper;
+import com.scm.helpers.Message;
+import com.scm.helpers.MessageType;
 import com.scm.services.ContactServiceInterface;
 import com.scm.services.UserService;
+
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import jakarta.websocket.Session;
 
 @Controller
 @RequestMapping("/user/contacts")
 public class ContactController {
-	
+
 	@Autowired
 	private ContactServiceInterface contactServiceInterface;
-	
+
 	@Autowired
 	private UserService userService;
 
 	@GetMapping("/add")
 	public String addContactView(Model model) {
-		
+
 		ContactForm contactForm = new ContactForm();
-		
+
 		model.addAttribute("contactForm", contactForm);
 		return "user/add_contact";
 	}
-	
+
 	@PostMapping("/processContacts")
-	public String saveContact(@ModelAttribute ContactForm contactForm, Authentication authentication) {
-		
-		//fatching current user then fetch user id from current user it set 
+	public String saveContact(@Valid @ModelAttribute ContactForm contactForm, BindingResult bindingResult,
+			Authentication authentication, HttpSession httpSession) {
+
+		// fatching current user then fetch user id from current user it set
 		String userName = Helper.getEmailOfLoggedInUser(authentication);
 		User user = userService.getUserByEmail(userName);
-		
-		
-		  //converting ContactForm into contact
-		  Contact contact = new Contact();
-		  contact.setUser(user); contact.setName(contactForm.getName());
-		  contact.setAddress(contactForm.getAddress());
-		  contact.setDescription(contactForm.getDescription());
-		  contact.setEmail(contactForm.getEmail());
-		  contact.setFavorite(contactForm.isFavorite());
-		  contact.setLinkdInLink(contactForm.getLinkdInLink());
-		  contact.setPhoneNumber(contactForm.getPhoneNumber());
-		  //contact.setPicture(contactForm.getPicture());
-		  contact.setWebsiteLink(contactForm.getWebsiteLink());
-		  
-		  
-		  
-		// save data into database 
-		 contactServiceInterface.saveContact(contact);
-		 
-		
-		
-		
+
+		// validation
+		if (bindingResult.hasErrors()) {
+			httpSession.setAttribute("message",
+					Message.builder().content("Please Correct the following errors").type(MessageType.red).build());
+			return "user/add_contact";
+		}
+
+		// converting ContactForm into contact
+		Contact contact = new Contact();
+		contact.setUser(user);
+		contact.setName(contactForm.getName());
+		contact.setAddress(contactForm.getAddress());
+		contact.setDescription(contactForm.getDescription());
+		contact.setEmail(contactForm.getEmail());
+		contact.setFavorite(contactForm.isFavorite());
+		contact.setLinkdInLink(contactForm.getLinkdInLink());
+		contact.setPhoneNumber(contactForm.getPhoneNumber());
+		// contact.setPicture(contactForm.getPicture());
+		contact.setWebsiteLink(contactForm.getWebsiteLink());
+
+		// save data into database
+		contactServiceInterface.saveContact(contact);
+
+		httpSession.setAttribute("message",
+				Message.builder().content("New Contact Added Sucessfully").type(MessageType.green).build());
 		return "redirect:/user/contacts/add";
 	}
 
--- src/main/java/com/scm/forms/ContactForm.java
+++ src/main/java/com/scm/forms/ContactForm.java
@@ -2,8 +2,10 @@ package com.scm.forms;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -18,9 +20,17 @@ public class ContactForm {
 	
 	
 	private String id;
+	@NotBlank(message = "Name is required")
 	private String name;
+	
+	@Email(message = "Invalid Email address")
 	private String email;
+	
+	@NotBlank(message = "phone number is required")
+	@Pattern(regexp = "^[0-9]{10}$", message = "Invalid Phone number")
 	private String phoneNumber;
+	
+	@NotBlank(message = "Address is required")
 	private String address;
 	private String description;
 	private boolean favorite;
